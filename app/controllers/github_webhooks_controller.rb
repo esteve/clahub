ec36315f0193e780153c3b4d5825cc19331dc3b1
Esteve Fernandez <esteve@osrfoundation.org>
2014-11-05 11:45:42 -0800

    checkin

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -14,6 +14,7 @@ class GithubWebhooksController < ApplicationController
       Rails.logger.info(payload.state)
       Rails.logger.info(payload.user_login)
       Rails.logger.info(payload.repo_name)
+      Rails.logger.info('================== STATUSES ==============')
       Rails.logger.info(payload.statuses)
     end
 
