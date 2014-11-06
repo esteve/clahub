16cbef66f1d28095d88aeecd5d4faa04792e575b
Esteve Fernandez <esteve@osrfoundation.org>
2014-11-05 16:50:56 -0800

    Added repo_agreement

--- app/models/github_status.rb
+++ app/models/github_status.rb
@@ -19,7 +19,14 @@ class GithubStatus
     @mash.sha
   end
 
+  def repo_agreement
+    @repo_agreement ||= Agreement.where({
+      user_name: user_login,
+      repo_name: repo_name
+    }).first
+  end
+
   def statuses
-    @mash.statuses
+    GithubRepos.new(repo_agreement.user).get_statuses(user_login, repo_name, sha)
   end
 end
